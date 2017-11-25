63b0f6af21b1b06e3c76e6cadc82c62a9850b757
Anders Eriksson <ders@stenkrossstudios.com>
2017-11-25 09:50:28 +0100

    Created c++ project

---
+++ Source/NewsGlobe/NewsGlobeGamemodeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewsGlobeGamemodeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWSGLOBE_API ANewsGlobeGamemodeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
